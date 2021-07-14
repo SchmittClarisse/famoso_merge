f7bb36279c9397b45dbeeee8d60e610987f2e2a8
SchmittClarisse <82055525+SchmittClarisse@users.noreply.github.com>
2021-07-14 22:52:40 +0200

    modif

--- NEXT/famoso_merge/merge.js
+++ NEXT/famoso_merge/merge.js
@@ -1,84 +1,45 @@
-const fs = require('fs');
+const merge = (leftArr, rightArr) => {
+   const output = [];
+   let leftIndex = 0;
+   let rightIndex = 0;
+   
+   while(leftIndex < leftArr.length && rightIndex < rightArr.length) {
+       const leftEl = leftArr[leftIndex];
+       const rightEl = rightArr[rightIndex];
 
-const fileName = process.argv[2];
+       if (leftEl < rightEl) {
+           output.push(leftEl);
+           leftIndex++;
+       } else {
+           output.push(rightEl);
+           rightIndex++;
+       }
+   }
+   return [...output, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)];
+};
 
-// Open & read the file with 'fs'
-fs.readFile(fileName, 'utf8', (error, data) => {
-  
-  // Log autommatic errors
-  if (error) {
-    console.error(error.message);
-    return ;
-  }
-
-  // check if the file is empty
-  if(data == null || data == undefined || data == '' ) return console.error('ERROR: Empty file');
-
-  // split & parse all data in integer
-  const list = (data.split(` `)).map(numb=>{
-    return parseInt(numb)
-  });
-
-  // Check if it's a number
-  if(list.includes(NaN)) return console.error('ERROR: Not A Number in file');
-
-
-  // Run Sorting Programms
-  new MergeSort(list)
-
-});
-
-////////////////////////////
-// Sort by Merge Sorting //
-///////////////////////////
-class MergeSort{
-  constructor(list){
-    this.list  = [...list]
-    this.count = 0
-    this.mergeSort(this.list)
-    this.viewResult()
-  }
-
-  mergeSort(list) {
-    // No need to sort the array if the array only has one element or empty
-    if (list.length <= 1) {
-      return list;
-    }
-    // In order to divide the array in half, we need to figure out the middle
-    const middle = Math.floor(list.length / 2);
-  
-    // This is where we will be dividing the array into left and right
-    const left  = list.slice(0, middle);
-    const right = list.slice(middle);
-  
-    // Using recursion to combine the left and right
-    return this.list = this.merge(this.mergeSort(left), this.mergeSort(right));
-  }
-
-  // Merge the two arrays: left and right
-  merge(left, right){
-    let resultArray = []
-    let leftIndex   = 0
-    let rightIndex  = 0;
-
-    // We will concatenate values into the resultArray in order
-    while (leftIndex < left.length && rightIndex < right.length) {
-      this.count++
-      if (left[leftIndex] < right[rightIndex]) {
-        resultArray.push(left[leftIndex]);
-        leftIndex++; // move left array cursor
-      } else {
-        resultArray.push(right[rightIndex]);
-        rightIndex++; // move right array cursor
-      }
+// recursive
+const mergeSort = array => {
+    if (array.length <= 1) {
+        return array;
     }
 
-    // We need to concat here because there will be one element remaining
-    // from either left OR the right
-    return resultArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-  }
+    const middleIndex = Math.floor(array.length / 2);
+    const leftArr = array.slice(0, middleIndex);
+    const rightArr = array.slice(middleIndex);
 
-  viewResult(){
-    console.log(`Tri fusion: ${this.count} comparaisons - [${this.list}]`)
-  }
-}
+    return merge(
+        mergeSort(leftArr),
+        mergeSort(rightArr)
+    );
+};
+
+console.log(mergeSort([1, 45, 98, 65, 45, 9, 7, 31]));
+
+//[1, 4, 2, 8, 345]
+//[1, 4, 2] [8, 345]
+//[1, 4] [2] [8] [345]
+//[1] [4] [2] [8] [345]
+//[1, 4] [2, 8] [345]
+//[1, 2, 4, 8] [345]
+//[1, 2, 4, 8, 345]
